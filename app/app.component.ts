fda0dc47a35f85769c4afcc627da0309a78aed00
Michael Treser <armikael2@gmail.com>
2016-10-27 16:56:32 +0300

    Wrapping main app with <main>, adding basic styles. Adding glyphicon fix for voter component.

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,21 +5,25 @@ import {VoteComponent} from './components/voter.component';
 
 @Component({
     selector: 'my-app',
-    template: `<h1>Angular 2 App</h1>
-               <courses></courses> 
-               <authors></authors>
-               <button class="btn-success" (click)="onClick($event)" >Submit</button>
-
-               <div>
-                   <h3 [style.color]="courseTitle ? 'gold' : 'gray'">Course Wanted: {{ courseTitle }}</h3>
-                   <input type="text" [(ngModel)]='courseTitle' />
-                   <input type="button" class="btn-danger" value="Clear" (click)="courseTitle=''" />
-               </div>
-               <voter></voter>
-               <article>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam aspernatur assumenda doloribus
-                enim maxime perspiciatis quia quisquam quo similique vel.
-               </article>
-               `, // Two-way data binding - [(banana in the box)] = (event-binding) + [property-binding]
+    template: `<main class="main">
+                   <h1>Angular 2 App</h1>
+                   <courses></courses> 
+                   <authors></authors>
+                   <button class="btn-success" (click)="onClick($event)" >Submit</button>
+    
+                   <div>
+                       <h3 [style.color]="courseTitle ? 'gold' : 'gray'">Course Wanted: {{ courseTitle }}</h3>
+                       <input type="text" [(ngModel)]='courseTitle' />
+                       <input type="button" class="btn-danger" value="Clear" (click)="courseTitle=''" />
+                   </div>
+                   
+                   <article>
+                    <voter></voter>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam aspernatur assumenda doloribus
+                    enim maxime perspiciatis quia quisquam quo similique vel.</p>
+                   </article>
+              </main> 
+              `, // Two-way data binding - [(banana in the box)] = (event-binding) + [property-binding]
     directives: [CoursesComponent, AuthorsComponent, VoteComponent]
 })
 export class AppComponent { 
--- app/components/voter.component.ts
+++ app/components/voter.component.ts
@@ -3,13 +3,15 @@ import {Component, Input} from 'angular2/core';
 @Component({
     selector: 'voter',
     template: `
-                <i class="glyphicon-menu-up" (click)="voteUp()">
-                </i>
-                
-                    {{ totalVotes }}
+                <div class="voter-block">
+                    <i class="glyphicon glyphicon-menu-up" (click)="voteUp()">
+                    </i>
                     
-                <i class="glyphicon-menu-down" (click)="voteDown">
-                </i>       
+                        <span>{{ totalVotes }}</span>
+                        
+                    <i class="glyphicon glyphicon-menu-down" (click)="voteDown">
+                    </i>       
+                </div>
               `
 })
 export class VoteComponent {
