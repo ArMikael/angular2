3cfe5863628786dea719140a7831339daba7a12c
Michael Treser <armikael2@gmail.com>
2016-12-26 00:33:17 +0300

    Cleaning boot.ts from router parts.

--- app/boot.js
+++ app/boot.js
@@ -1,7 +1,7 @@
 System.register(['angular2/platform/browser', './app.component', 'angular2/router', 'angular2/core'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
-    var browser_1, app_component_1, router_1, router_2, core_1;
+    var browser_1, app_component_1, router_1, core_1;
     return {
         setters:[
             function (browser_1_1) {
@@ -12,13 +12,12 @@ System.register(['angular2/platform/browser', './app.component', 'angular2/route
             },
             function (router_1_1) {
                 router_1 = router_1_1;
-                router_2 = router_1_1;
             },
             function (core_1_1) {
                 core_1 = core_1_1;
             }],
         execute: function() {
-            browser_1.bootstrap(app_component_1.AppComponent, [router_1.ROUTER_PROVIDERS, core_1.provide(router_2.APP_BASE_HREF, { useValue: '/' })]);
+            browser_1.bootstrap(app_component_1.AppComponent, [core_1.provide(router_1.APP_BASE_HREF, { useValue: '/' })]);
         }
     }
 });
--- app/boot.ts
+++ app/boot.ts
@@ -1,7 +1,6 @@
 import {bootstrap}    from 'angular2/platform/browser';
 import {AppComponent} from './app.component';
-import {ROUTER_PROVIDERS} from 'angular2/router';
 import {APP_BASE_HREF} from 'angular2/router';
 import {provide} from 'angular2/core';
 
-bootstrap(AppComponent, [ROUTER_PROVIDERS, provide(APP_BASE_HREF, {useValue : '/' })]);
+bootstrap(AppComponent, [provide(APP_BASE_HREF, {useValue : '/' })]);
