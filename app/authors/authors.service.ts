9d130a1b722affc860be4b048a065081d344d6d1
Michael Treser <armikael2@gmail.com>
2017-03-19 14:05:03 +0200

    Added getPosts method to authorsService.js. Service was changed to Injectable with call requests to jsonplaceholder.

--- app/authors/authors.service.ts
+++ app/authors/authors.service.ts
@@ -1,11 +1,26 @@
+import {Injectable} from 'angular2/core';
+import {Http} from 'angular2/http';
+
+@Injectable()
 export class AuthorsService {
+    private _url = "http://jsonplaceholder.typicode.com/posts";
+
+    constructor(private _http: Http){
+
+    }
+
     getAuthors(){
         return [
-            {name: 'Uwe Rosenberg', id: 101},
-            {name: 'Vlaada Chvatil', id: 102},
-            {name: 'Ignazi Tzevichek', id: 103},
-            {name: 'Mac Herds', id: 104},
-            {name: 'Stefan Feld', id: 105}
+            {name: 'Uwe Rosenberg', id: 1},
+            {name: 'Vlaada Chvatil', id: 2},
+            {name: 'Ignazi Tzevichek', id: 3},
+            {name: 'Mac Herds', id: 4},
+            {name: 'Stefan Feld', id: 5}
         ]
+    }
+
+    getPost(id){
+        return this._http.get(this._url + "/?userId=" + id)
+            .map(res => res.json());
     }
 }
